eb2940fde675262dbbedf212a29ff9fc3858e12a
7Lena555 <Olena.lapa@gmail.com>
2023-01-03 00:02:44 +0200

    1 commit

---
+++ src/main/java/test_olena_lapa/LogoTest.java
@@ -0,0 +1,39 @@
+package test_olena_lapa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class LogoTest {
+
+
+    public boolean open() {
+        Selenide
+                .open("http://www.google.com");
+        $x("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]")
+                .setValue("test automation")
+                .sendKeys(Keys.ENTER);
+        return this;
+        String String;
+        final var s = String = "searchTerm";
+        {
+            String inputFieldPath = "//tbody/tr[1]/td[6]/a[1]";
+            $x(inputFieldPath).sendKeys("searchTerm");
+            $x("//Page 5").click();
+            return new LogoTest().open();
+            public boolean isGoogleLogoVisible () {
+            try {
+                $x("//body/div[1]/div[2]/div[1]/img[1]").shouldBe(Condition.visible);
+                return true;
+
+            } catch (AssertionError ex) {
+                return false;
+            }
+
+        }
+
+
+
+    }
---
+++ src/test/java/test_olena_lapa/GoogleSearchButtonTestTest.java
@@ -0,0 +1,7 @@
+package test_olena_lapa;
+
+import junit.framework.TestCase;
+
+public class GoogleSearchButtonTestTest extends TestCase {
+
+}
